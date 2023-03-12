7a1404b38ff643e3b3e1755d320c1b4968f8e4e6
benji-35 <benjamin.delvert@epitech.eu>
2023-03-12 21:12:49 +0100

    FIX : function name for revser string

--- src/string/string_reverse.c
+++ src/string/string_reverse.c
@@ -7,11 +7,21 @@
 
 #include "kapstring.h"
 
-string str_reverse(cstring str) {
-    string result = str_create_empty();
-    ksize_t size = str_len(str);
+void str_reverse(string str) {
+    int len = str_len(str);
+    int i = 0;
+    char tmp = 0;
 
-    for (ksize_t i = size - 1; i > 0; i--)
-        str_add_char(&result, str[i]);
-    return (result);
+    for (i = 0; i < len / 2; i++) {
+        tmp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = tmp;
+    }
+}
+
+string str_reverse_copy(cstring str) {
+    string cpy = str_copy(str);
+
+    str_reverse(cpy);
+    return(cpy);
 }
