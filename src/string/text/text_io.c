0886f9e4cf4a1d7ca703ef690bc56191729075fe
benji-35 <benjamin.delvert@epitech.eu>
2023-03-21 18:48:53 +0100

    FIX : spliting readed text

--- src/string/text/text_io.c
+++ src/string/text/text_io.c
@@ -16,6 +16,11 @@ void text_write(ctext_t text, cstring path, bool append) {
 text_t text_read(cstring path) {
     string str = str_read(path);
     text_t text = str_split_str(str, "\r\n");
+
+    if (text->size == 1 && str_contains_char(text_get_line(text, 0), '\n')) {
+        text_destroy(text);
+        text = str_split_str(str, "\n");
+    }
     kapfree(str);
     return text;
 }
